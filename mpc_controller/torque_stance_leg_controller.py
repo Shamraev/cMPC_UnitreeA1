e9f2c507d3d28d9d137c47ec755fccb96f930337
Shamraev <ros-user@example.com>
2022-05-03 09:42:27 +0200

    linear slip reference trajectory for COM out of MPC. In states z and z_dot, in x and x_dot states not working

--- mpc_controller/torque_stance_leg_controller.py
+++ mpc_controller/torque_stance_leg_controller.py
@@ -15,6 +15,7 @@ os.sys.path.insert(0, parentdir)
 from typing import Any, Sequence, Tuple
 
 import numpy as np
+from numpy import cos,cosh,sin,sinh
 import pybullet as p  # pytype: disable=import-error
 
 try:
@@ -47,6 +48,68 @@ _MPC_WEIGHTS = (5, 5, 0.2, 0, 0, 10, 0., 0., 1., 1., 1., 0., 0)
 _PLANNING_HORIZON_STEPS = 10
 _PLANNING_TIMESTEP = 0.025
 
+class SLIP_generator():
+  def __init__(self, k, m, duration, time_step):
+    self._g = 9.81
+    self._k = k
+    self._m = m
+    self._des_z0 = 0.26
+    self._wx = np.sqrt(self._g/(self._des_z0*1.2))
+    self._wz = np.sqrt(self._k/self._m)
+    self._Ts = time_step
+    self._duration = duration
+
+    self._z0 = 0
+    self._z_dot0 = 0
+    self._x0 = 0
+    self._x_dot0 = 0
+
+    self.state_z = np.array([[self._z0], [self._z_dot0]])
+    self.state_x = np.array([[self._x0], [self._x_dot0]])
+    self.started_cycle = False
+
+    w = self._wz
+    Ts = self._Ts
+    self.A_z = np.array([ [cos(w*Ts), sin(w*Ts)/w], 
+                        [-w*sin(w*Ts), cos(w*Ts)]])
+    self.B_z = np.array([[1-cos(w*Ts)], [w*sin(w*Ts)]])
+
+  def Reset(self, z0, z_dot0, x0, x_dot0):
+    self._z0 = z0
+    self._z_dot0 = z_dot0
+    self._x0 = x0
+    self._x_dot0 = x_dot0
+
+    self.state_z = np.array([[self._z0], [self._z_dot0]])
+    self.state_x = np.array([[self._x0], [self._x_dot0]])
+    wz = self._wz
+    wx = self._wx
+    Ts = self._Ts
+    self.A_z = np.array([ [cos(wz*Ts), sin(wz*Ts)/wz], 
+                        [-wz*sin(wz*Ts), cos(wz*Ts)]])
+    self.B_z = np.array([[1-cos(wz*Ts)], [wz*sin(wz*Ts)]])
+
+    self.A_x = np.array([ [cosh(wx*Ts), sinh(wx*Ts)/wx], 
+                        [wx*sinh(wx*Ts), cosh(wx*Ts)]])
+
+  def get_traj_z(self):
+    u = self._des_z0 -self._g/(self._wz**2) # 0.23 0.25
+    self.state_z = self.A_z@self.state_z + self.B_z*u
+
+    return self.state_z
+
+  def get_traj_x(self):
+    self.state_x = self.A_x@self.state_x
+
+    return self.state_x
+
+
+  def get_traj(self):
+    z = self.get_traj_z()
+    x = self.get_traj_x()
+    return z, x
+
+  
 
 class TorqueStanceLegController(leg_controller.LegController):
   """A torque based stance leg controller framework.
@@ -113,6 +176,13 @@ class TorqueStanceLegController(leg_controller.LegController):
         qp_solver
         
     )
+    self._SLIP = SLIP_generator(4000, body_mass, self._gait_generator.stance_duration[0], 0.002) # ?? # 
+    self.last_foot_contact_states = np.array(
+        [True, True, True, True],
+        dtype=np.int32)
+    self.start_cotntact_t = self._robot.GetTimeSinceReset()
+    self.duration_of_contact = 0.2
+    self.last_t = self._robot.GetTimeSinceReset()
 
   def reset(self, current_time):
     del current_time
@@ -120,8 +190,26 @@ class TorqueStanceLegController(leg_controller.LegController):
   def update(self, current_time):
     del current_time
 
+  def _estimate_robot_height(self, contacts):
+    if np.sum(contacts) == 0:
+      # All foot in air, no way to estimate
+      return self._desired_body_height
+    else:
+      base_orientation = self._robot.GetBaseOrientation()
+      rot_mat = self._robot.pybullet_client.getMatrixFromQuaternion(
+          base_orientation)
+      rot_mat = np.array(rot_mat).reshape((3, 3))
+
+      foot_positions = self._robot.GetFootPositionsInBaseFrame()
+      foot_positions_world_frame = (rot_mat.dot(foot_positions.T)).T
+      # pylint: disable=unsubscriptable-object
+      useful_heights = contacts * (-foot_positions_world_frame[:, 2])
+      return np.sum(useful_heights) / np.sum(contacts)
+
   def get_action(self):
     """Computes the torque for stance legs."""
+    dt = self._robot.GetTimeSinceReset()-self.last_t
+    #print('dt: ', dt)
     desired_com_position = np.array((0., 0., self._desired_body_height),
                                     dtype=np.float64)
     desired_com_velocity = np.array(
@@ -140,13 +228,43 @@ class TorqueStanceLegController(leg_controller.LegController):
                                   dtype=np.float64)
     com_roll_pitch_yaw[2] = 0
 
+    ## SLIP traj for COM
+    # One spring only
+    start_contact = self.last_foot_contact_states[0]!=foot_contact_state[0] and foot_contact_state[0]
+    end_contact = self.last_foot_contact_states[0]!=foot_contact_state[0] and not foot_contact_state[0]
+
+    contacts = np.array(
+    [(leg_state in (gait_generator_lib.LegState.STANCE,
+                    gait_generator_lib.LegState.EARLY_CONTACT))
+      for leg_state in self._gait_generator.desired_leg_state],
+    dtype=np.int32)
+
+    x = 0
+    z = self._estimate_robot_height(contacts)
+    #z = self._robot.GetBasePosition()[2] # !! #
+    z_dot, x_dot = self._state_estimator.com_velocity_body_frame[2], self.desired_speed[0]
+    if start_contact:
+      self._SLIP.Reset(z, z_dot, x, x_dot)
+      self._SLIP.started_cycle = True
+    if end_contact:
+      self._SLIP.started_cycle = False
+
+    #
+    if self._SLIP.started_cycle:
+      z, x = self._SLIP.get_traj()
+      desired_com_position[2] = z[0]
+      desired_com_velocity[2] = z[1]
+      # desired_com_velocity[0] = x[1]
+      
+    ## SLIP traj for COM END
+
     #predicted_contact_forces=[0]*self._num_legs*_FORCE_DIMENSION
     # print("Com Vel: {}".format(self._state_estimator.com_velocity_body_frame))
     # print("Com RPY: {}".format(self._robot.GetBaseRollPitchYawRate()))
     # print("Com RPY Rate: {}".format(self._robot.GetBaseRollPitchYawRate()))
     p.submitProfileTiming("predicted_contact_forces")
     predicted_contact_forces = self._cpp_mpc.compute_contact_forces(
-        [0],  #com_position
+        [0],  #com_position //??//
         np.asarray(self._state_estimator.com_velocity_body_frame,
                    dtype=np.float64),  #com_velocity
         np.array(com_roll_pitch_yaw, dtype=np.float64),  #com_roll_pitch_yaw
@@ -186,5 +304,6 @@ class TorqueStanceLegController(leg_controller.LegController):
       for joint_id, torque in motor_torques.items():
         action[joint_id] = (0, 0, 0, 0, torque)
 
+    self.last_foot_contact_states = foot_contact_state
+    self.last_t = self._robot.GetTimeSinceReset()
     return action, contact_forces, desired_com_position, desired_com_velocity
-
